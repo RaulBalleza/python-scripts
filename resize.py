299382bc407699f7705540b1fa1d59f10a7b3288
raulballeza <1730425@upv.edu.mx>
2020-03-02 09:10:47 -0600

    actualizado

--- resize.py
+++ resize.py
@@ -6,15 +6,22 @@ img = cv2.imread(sys.argv[1], cv2.IMREAD_UNCHANGED)
  
 print('Original Dimensions : ',img.shape)
  
-width = int(sys.argv[2])
-height = int(sys.argv[3])
+width = int(sys.argv[3])
+height = int(sys.argv[2])
 dim = (width, height)
- 
+
+name = sys.argv[1].split("/")
+name = name[-1]
+
+#extension = name.split(".")
+#extension = extension[-1]
+
 # resize image
 resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
- 
+path = os.getcwd() + "/resized_"+name
+print("Saved on",path)
 print('Resized Dimensions : ',resized.shape)
-cv2.imwrite("resized_"+sys.argv[1],resized)
+cv2.imwrite(path,resized)
 #cv2.imshow("Resized image", resized)
 #cv2.waitKey(0)
 #cv2.destroyAllWindows()
